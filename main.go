66006a05375d96d71154df8ce3595f3c4b9eec44
Andrea Funtò <d093154@xpia040.utenze.bankit.it>
2025-01-30 16:00:12 +0100

    Added explain subcommand with improved formatting

--- main.go
+++ main.go
@@ -22,14 +22,96 @@ type category struct {
 	key     string
 	count   int
 	regexes []*regexp.Regexp
+	help    []string
 	color   func(a ...any) string
+}
+
+var categories = []category{
+	{
+		key: "Contributo scarso",
+		regexes: []*regexp.Regexp{
+			regexp.MustCompile(`(?i)\sridott(a|e|o|i)`),
+			regexp.MustCompile(`(?i)\sscars(a|e|o|i)`),
+			regexp.MustCompile(`(?i)\snon apprezzabil(e|i)`),
+			regexp.MustCompile(`(?i)\srar(a|e|o|i)`),
+		},
+		help:  []string{"ridott*", "scars*", "non appezzabil*", "rar*"},
+		color: color.New(color.FgMagenta).SprintFunc(),
+	},
+	{
+		key: "Contributo limitato",
+		regexes: []*regexp.Regexp{
+			regexp.MustCompile(`(?i)\slimitat(o|a|i|e)`),
+			regexp.MustCompile(`(?i)\smoderat(o|a|i|e)`),
+			regexp.MustCompile(`(?i)\sparzial(e|i)`),
+			regexp.MustCompile(`(?i)\sliev(e|i)`),
+			regexp.MustCompile(`(?i)\spoc(o|a|hi|he)`),
+			regexp.MustCompile(`(?i)\scontenut(o|a|i|e)`),
+			regexp.MustCompile(`(?i)\snon consistent(i|e)`),
+		},
+		help:  []string{"limitat*", "moderat*", "parzial*", "liev*", "poc*", "contenut*", "non consistent*"},
+		color: color.New(color.FgRed).SprintFunc(),
+	},
+	{
+		key: "Contributo apprezzabile",
+		regexes: []*regexp.Regexp{
+			regexp.MustCompile(`(?i)\sdiscret(o|a|i|e)`),
+			regexp.MustCompile(`(?i)\sapprezzabil(e|i)`),
+			regexp.MustCompile(`(?i)\smaggior parte`),
+			regexp.MustCompile(`(?i)\sper lo pi(ù|u')`),
+			regexp.MustCompile(`(?i)\sconsistent(e|i)`),
+			regexp.MustCompile(`(?i)\sbuon(o|a|i|e)`),
+			regexp.MustCompile(`(?i)\sspesso`),
+			regexp.MustCompile(`(?i)\sattent(o|a|i|e)`),
+		},
+		help:  []string{"discret*", "apprezzabil*", "maggior parte", "per lo più", "consistent*", "buon*", "spesso", "attent*"},
+		color: color.New(color.FgYellow).SprintFunc(),
+	},
+	{
+		key: "Contributo significativo",
+		regexes: []*regexp.Regexp{
+			regexp.MustCompile(`(?i)\ssignificativ(o|a|i|e)`),
+			regexp.MustCompile(`(?i)\snotevol(e|i)`),
+			regexp.MustCompile(`(?i)\sconsiderevol(e|i)`),
+			regexp.MustCompile(`(?i)\sfort(e|i)`),
+			regexp.MustCompile(`(?i)\selevat(o|a|i|e)`),
+			regexp.MustCompile(`(?i)\srilevant(e|i)`),
+			regexp.MustCompile(`(?i)\sottim(o|a|i|e)`),
+			regexp.MustCompile(`(?i)\sfondamental(e|i)`),
+			regexp.MustCompile(`(?i)\sdeterminant(e|i)`),
+			regexp.MustCompile(`(?i)\sapprezzabil(e|i)`),
+		},
+		help:  []string{"significativ*", "notevol*", "considerevol*", "fort*", "elevat*", "rilevant*", "ottim*", "fondamental*", "determinant*", "apprezzabil*"},
+		color: color.New(color.FgGreen).SprintFunc(),
+	},
+	{
+		key: "Impegno, competenza, managerialità",
+		regexes: []*regexp.Regexp{
+			regexp.MustCompile(`(?i)\sdot[e|i] managerial(e|i)`),
+			regexp.MustCompile(`(?i)\scapacit[à|a'] organizzativ(a|e)`),
+			regexp.MustCompile(`(?i)\scompetenz(a|e)] tecnic[a|he]`),
+		},
+		help:  []string{"dot* managerial*", "capacità organizzativ*", "competenz* tecnic*"},
+		color: color.New(color.FgBlue).SprintFunc(),
+	},
 }
 
 func main() {
 
-	if len(os.Args) == 2 && valueIn(os.Args[1], "version", "-version", "--version", "ver", "-ver", "--ver", "v", "-v", "--v") {
-		version.Print(os.Stdout)
-		return
+	if len(os.Args) == 2 {
+		if valueIn(os.Args[1], "version", "-version", "--version", "ver", "-ver", "--ver", "v", "-v", "--v") {
+			version.Print(os.Stdout)
+			return
+		} else if valueIn(os.Args[1], "explain", "-explain", "--explain", "e", "-e", "--e", "show", "-show", "--show", "s", "-s", "--s") {
+			for _, category := range categories {
+				fmt.Printf("%-44s: ", category.color(category.key))
+				for _, h := range category.help {
+					fmt.Printf("%-35s", category.color(h))
+				}
+				fmt.Println()
+			}
+			return
+		}
 	}
 
 	options := Options{}
@@ -65,71 +147,6 @@ func main() {
 	}
 
 	data := string(d)
-
-	categories := []category{
-		{
-			key: "Contributo scarso",
-			regexes: []*regexp.Regexp{
-				regexp.MustCompile(`(?i)\sridott(a|e|o|i)`),
-				regexp.MustCompile(`(?i)\sscars(a|e|o|i)`),
-				regexp.MustCompile(`(?i)\snon apprezzabil(e|i)`),
-				regexp.MustCompile(`(?i)\srar(a|e|o|i)`),
-			},
-			color: color.New(color.FgMagenta).SprintFunc(),
-		},
-		{
-			key: "Contributo limitato",
-			regexes: []*regexp.Regexp{
-				regexp.MustCompile(`(?i)\slimitat(o|a|i|e)`),
-				regexp.MustCompile(`(?i)\smoderat(o|a|i|e)`),
-				regexp.MustCompile(`(?i)\sparzial(e|i)`),
-				regexp.MustCompile(`(?i)\sliev(e|i)`),
-				regexp.MustCompile(`(?i)\spoc(o|a|hi|he)`),
-				regexp.MustCompile(`(?i)\scontenut(o|a|i|e)`),
-				regexp.MustCompile(`(?i)\snon consistent(i|e)`),
-			},
-			color: color.New(color.FgRed).SprintFunc(),
-		},
-		{
-			key: "Contributo apprezzabile",
-			regexes: []*regexp.Regexp{
-				regexp.MustCompile(`(?i)\sdiscret(o|a|i|e)`),
-				regexp.MustCompile(`(?i)\sapprezzabil(e|i)`),
-				regexp.MustCompile(`(?i)\smaggior parte`),
-				regexp.MustCompile(`(?i)\sper lo pi(ù|u')`),
-				regexp.MustCompile(`(?i)\sconsistent(e|i)`),
-				regexp.MustCompile(`(?i)\sbuon(o|a|i|e)`),
-				regexp.MustCompile(`(?i)\sspesso`),
-				regexp.MustCompile(`(?i)\sattent(o|a|i|e)`),
-			},
-			color: color.New(color.FgYellow).SprintFunc(),
-		},
-		{
-			key: "Contributo significativo",
-			regexes: []*regexp.Regexp{
-				regexp.MustCompile(`(?i)\ssignificativ(o|a|i|e)`),
-				regexp.MustCompile(`(?i)\snotevol(e|i)`),
-				regexp.MustCompile(`(?i)\sconsiderevol(e|i)`),
-				regexp.MustCompile(`(?i)\sfort(e|i)`),
-				regexp.MustCompile(`(?i)\selevat(o|a|i|e)`),
-				regexp.MustCompile(`(?i)\srilevant(e|i)`),
-				regexp.MustCompile(`(?i)\sottim(o|a|i|e)`),
-				regexp.MustCompile(`(?i)\sfondamental(e|i)`),
-				regexp.MustCompile(`(?i)\sdeterminant(e|i)`),
-				regexp.MustCompile(`(?i)\sapprezzabil(e|i)`),
-			},
-			color: color.New(color.FgGreen).SprintFunc(),
-		},
-		{
-			key: "Impegno, competenza, managerialità",
-			regexes: []*regexp.Regexp{
-				regexp.MustCompile(`(?i)\sdot[e|i] managerial(e|i)`),
-				regexp.MustCompile(`(?i)\scapacit[à|a'] organizzativ(a|e)`),
-				regexp.MustCompile(`(?i)\scompetenz(a|e)] tecnic[a|he]`),
-			},
-			color: color.New(color.FgBlue).SprintFunc(),
-		},
-	}
 
 	var buffer bytes.Buffer
 	for i, category := range categories {
